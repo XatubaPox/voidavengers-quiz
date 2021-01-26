4895b9b0ee0d88e31d3a3cf00fe11248b45f530b
Gabriel D'Nillo <gabrieldnillo99@gmail.com>
2021-01-25 21:22:32 -0300

    add some meta tags

--- src/components/QuizHead/index.js
+++ src/components/QuizHead/index.js
@@ -6,6 +6,11 @@ export default function QuizHead() {
         <Head>
             <title>Quiz Void Avengers</title>
             <meta property="og:image" content={db.bg}/>
+            <meta property="og:url" content="https://voidavengers-quiz.xatubapox.vercel.app/" />
+            <meta property="og:type" content="website" />
+
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:image" content={db.bg}/>
         </Head>
     )
 }
