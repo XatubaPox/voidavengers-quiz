2626e595ea9090da99066a04dfaa71153ea3c6c4
Gabriel D'Nillo <gabrieldnillo99@gmail.com>
2021-01-27 15:14:22 -0300

    change tags input and button in html to jsx

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,10 @@ import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
-export const QuizContainer = styled.div`
+const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -33,23 +35,21 @@ export default function Home() {
             </Widget.Header>
 
             <Widget.Content>
-              <form onSubmit={function (infoEvent) {
+              <form onSubmit={(infoEvent) => {
                 infoEvent.preventDefault();
                 router.push(`/quiz?name=${name}`);
                 console.log('Fazendo submissão');
               }}
               >
-                <input
-                  onChange={function (infoEvent) {
-                    console.log(infoEvent.target.value);
-                    setName(infoEvent.target.value);
-                  }}
+                <Input
+                  name="user"
+                  onChange={(infoEvent) => setName(infoEvent.target.value)}
                   placeholder="Informe seu nick:"
+                  value={name}
                 />
-                <button type="submit" disabled={name.length === 0}>
-                  Jogar
-                  {name}
-                </button>
+                <Button type="submit" disabled={name.length === 0}>
+                  {`Jogar ${name}`}
+                </Button>
               </form>
             </Widget.Content>
           </Widget>
