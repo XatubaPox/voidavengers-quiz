58098ebb9a3e799c02765af8c0552c1c17c59727
Gabriel D'Nillo <gabrieldnillo99@gmail.com>
2021-01-29 17:18:08 -0300

    add server quiz external

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,9 +1,44 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz';
 
-export default function QuizDaGaleraPage() {
+export default function QuizDaGaleraPage({ dbExternal }) {
   return (
-    <div>
-      Desafio...
-    </div>
+    <ThemeProvider theme={dbExternal.theme}>
+      <QuizScreen
+        externalQuestions={dbExternal.questions}
+        externalBg={dbExternal.bg}
+      />
+    </ThemeProvider>
+    // <pre style={{ color: 'black' }}>
+    //   {JSON.stringify(dbExternal.questions, null, 4)}
+    // </pre>
   );
+}
+
+export async function getServerSideProps(context) {
+  const [projectName, githubUser] = context.query.id.split('___');
+  const dbExternal = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+    .then((responseServer) => {
+      if (responseServer.ok) {
+        return responseServer.json();
+      }
+
+      throw new Error('Falha em pegar os dados');
+    })
+    .then((responseConvertObject) => responseConvertObject)
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    });
+
+  // console.log('Banco de dados externo:', dbExternal);
+  // console.log('Infos Next:', context.query.id);
+
+  return {
+    props: {
+      dbExternal,
+    },
+  };
 }
