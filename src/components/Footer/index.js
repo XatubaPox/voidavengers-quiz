2b782c216e40186ba3bf196e7d2efeecaedc1ce4
Gabriel D'Nillo <gabrieldnillo99@gmail.com>
2021-01-29 17:21:24 -0300

    add atribute alt in tag img

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -30,7 +30,7 @@ export default function Footer(props) {
     // eslint-disable-next-line react/jsx-props-no-spreading
     <FooterWrapper {...props}>
       <a href="https://discord.gg/qS5P2bqHGb">
-        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1a523cb6-7366-4f8d-a46c-7c944da1854b/d8n9x2g-4420b2ea-ed1c-40f6-8064-d6021763e0f9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMWE1MjNjYjYtNzM2Ni00ZjhkLWE0NmMtN2M5NDRkYTE4NTRiXC9kOG45eDJnLTQ0MjBiMmVhLWVkMWMtNDBmNi04MDY0LWQ2MDIxNzYzZTBmOS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.VEIRwUcilpgtZ7lkQAcRDeVvHrNsgRnulZZegyDxNTA" />
+        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1a523cb6-7366-4f8d-a46c-7c944da1854b/d8n9x2g-4420b2ea-ed1c-40f6-8064-d6021763e0f9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMWE1MjNjYjYtNzM2Ni00ZjhkLWE0NmMtN2M5NDRkYTE4NTRiXC9kOG45eDJnLTQ0MjBiMmVhLWVkMWMtNDBmNi04MDY0LWQ2MDIxNzYzZTBmOS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.VEIRwUcilpgtZ7lkQAcRDeVvHrNsgRnulZZegyDxNTA" alt="Discord Void Avengers" />
       </a>
       <p>
         <a href="https://discord.gg/qS5P2bqHGb">
